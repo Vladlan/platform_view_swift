67964db91371d7befc2a6b7d67f61856dfd2b84e
Marina <marina@Marinas-iMac.local>
2019-04-22 15:22:46 -0700

    moved player to AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,11 +4,55 @@
 
 import UIKit
 import Flutter
+import MediaPlayer
+import FRadioPlayer
+
+struct Track {
+  var artist: String?
+  var name: String?
+  var image: UIImage?
+
+  init(artist: String? = nil, name: String? = nil, image: UIImage? = nil) {
+    self.name = name
+    self.artist = artist
+    self.image = image
+  }
+}
+
+struct Station {
+  let name: String
+  let detail: String
+  let url: URL
+  var image: URL?
+
+  init(name: String, detail: String, url: URL, image: URL? = nil) {
+    self.name = name
+    self.detail = detail
+    self.url = url
+    self.image = image
+  }
+}
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate, PlatformViewControllerDelegate
 {
   var flutterResult: FlutterResult?
+
+  var track: Track? {
+    didSet {
+      updateNowPlaying(with: track)
+    }
+  }
+
+  // Singleton ref to pla¡¡yer
+  let player: FRadioPlayer = FRadioPlayer.shared
+
+  // List of stations
+  let stations = [Station(name: "Newport Folk Radio",
+          detail: "Are you ready to Folk?",
+          url: URL(string: "https://s2.radio.co/se91d38a33/listen")!,
+          image: URL(string: "http://xata44.by/sites/default/files/logo-radio.png")!),
+  ]
 
   override func application(
     _ application: UIApplication,
@@ -17,18 +61,28 @@ import Flutter
     let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
     let channel = FlutterMethodChannel.init(name: "samples.flutter.io/platform_view_swift", binaryMessenger: controller)
 
+    player.delegate = self
+
+    player.radioURL = URL(string: "https://s2.radio.co/se91d38a33/listen")
+    player.isAutoPlay = true
+    player.enableArtwork = true
+    player.artworkSize = 600
+    player.togglePlaying()
+
+    setupRemoteTransportControls()
+
     channel.setMethodCallHandler({
       (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
       if ("switchView" == call.method) {
         self.flutterResult = result
         print("switchView")
-        let platformViewController = controller.storyboard?.instantiateViewController(withIdentifier: "PlatformView") as! PlatformViewController
-        platformViewController.counter = call.arguments as! Int
-        platformViewController.delegate = self
-
-        let navigationController = UINavigationController(rootViewController: platformViewController)
-        navigationController.navigationBar.topItem?.title = "Platform View1"
-        controller.present(navigationController, animated: true, completion: nil)
+//        let platformViewController = controller.storyboard?.instantiateViewController(withIdentifier: "PlatformView") as! PlatformViewController
+//        platformViewController.counter = call.arguments as! Int
+//        platformViewController.delegate = self
+//
+//        let navigationController = UINavigationController(rootViewController: platformViewController)
+//        navigationController.navigationBar.topItem?.title = "Platform View1"
+//        controller.present(navigationController, animated: true, completion: nil)
       } else {
         result(FlutterMethodNotImplemented)
       }
@@ -42,7 +96,7 @@ import Flutter
         return
       }
       print("togglePlaying()")
-//      player.togglePlaying()
+      self?.player.togglePlaying()
     })
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
@@ -50,5 +104,131 @@ import Flutter
 
   func didUpdateCounter(counter: Int) {
     flutterResult?(counter)
+  }
+}
+
+// MARK: - FRadioPlayerDelegate
+extension AppDelegate: FRadioPlayerDelegate {
+  func radioPlayer(_ player: FRadioPlayer, playerStateDidChange state: FRadioPlayerState) {
+    print("0")
+    print(state)
+  }
+
+  func radioPlayer(_ player: FRadioPlayer, playbackStateDidChange state: FRadioPlaybackState) {
+//    playButton.isSelected = player.isPlaying
+    print(state)
+    print("1")
+  }
+
+  func radioPlayer(_ player: FRadioPlayer, metadataDidChange artistName: String?, trackName: String?) {
+    print("2")
+    print("artistName:")
+    print(artistName)
+    print("trackName:")
+    print(trackName)
+    track = Track(artist: artistName, name: trackName)
+  }
+
+  func radioPlayer(_ player: FRadioPlayer, itemDidChange url: URL?) {
+    print("3")
+    print("url:")
+    print(url)
+    track = nil
+  }
+
+  func radioPlayer(_ player: FRadioPlayer, metadataDidChange rawValue: String?) {
+    print("4")
+    print("rawValue:")
+    print(rawValue)
+//      infoContainer.isHidden = (rawValue == nil)
+  }
+
+  func radioPlayer(_ player: FRadioPlayer, artworkDidChange artworkURL: URL?) {
+    print("5")
+    print("artworkURL:")
+    print(artworkURL)
+    // Please note that the following example is for demonstration purposes only, consider using asynchronous network calls to set the image from a URL.
+    guard let artworkURL = artworkURL, let data = try? Data(contentsOf: artworkURL) else {
+//      artworkImageView.image = stations[selectedIndex].image
+      return
+    }
+    track?.image = UIImage(data: data)
+//    artworkImageView.image = track?.image
+    updateNowPlaying(with: track)
+  }
+}
+
+// MARK: - Remote Controls / Lock screen
+
+extension AppDelegate {
+
+  func setupRemoteTransportControls() {
+    print("setupRemoteTransportControls()")
+    // Get the shared MPRemoteCommandCenter
+    let commandCenter = MPRemoteCommandCenter.shared()
+
+    // Add handler for Play Command
+    commandCenter.playCommand.addTarget { [unowned self] event in
+      if self.player.rate == 0.0 {
+        self.player.play()
+        return .success
+      }
+      return .commandFailed
+    }
+
+    // Add handler for Pause Command
+    commandCenter.pauseCommand.addTarget { [unowned self] event in
+      if self.player.rate == 1.0 {
+        self.player.pause()
+        return .success
+      }
+      return .commandFailed
+    }
+
+    // Add handler for Next Command
+    commandCenter.nextTrackCommand.addTarget { [unowned self] event in
+//      self.next()
+      return .success
+    }
+
+    // Add handler for Previous Command
+    commandCenter.previousTrackCommand.addTarget { [unowned self] event in
+//      self.previous()
+      return .success
+    }
+  }
+
+  func updateNowPlaying(with track: Track?) {
+    print("updateNowPlaying(with track: Track?)")
+
+    // Define Now Playing Info
+    var nowPlayingInfo = [String : Any]()
+
+    if let artist = track?.artist {
+      nowPlayingInfo[MPMediaItemPropertyArtist] = artist
+    }
+
+    nowPlayingInfo[MPMediaItemPropertyTitle] = track?.name ?? stations[0].name
+
+    if #available(iOS 10.0, *) {
+      if let image = track?.image {
+        nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { _ -> UIImage in
+          return image
+        })
+      }
+    } else {
+
+    }
+
+    // Set the metadata
+    MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+  }
+}
+
+// MARK: - UINavigationController
+
+extension UINavigationController {
+  open override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
   }
 }
--- ios/Runner/PlatformViewController.swift
+++ ios/Runner/PlatformViewController.swift
@@ -4,34 +4,34 @@
 
 import UIKit
 import Foundation
-import MediaPlayer
-import FRadioPlayer
+//import MediaPlayer
+//import FRadioPlayer
 
-struct Track {
-  var artist: String?
-  var name: String?
-  var image: UIImage?
-
-  init(artist: String? = nil, name: String? = nil, image: UIImage? = nil) {
-    self.name = name
-    self.artist = artist
-    self.image = image
-  }
-}
-
-struct Station {
-  let name: String
-  let detail: String
-  let url: URL
-  var image: URL?
-
-  init(name: String, detail: String, url: URL, image: URL? = nil) {
-    self.name = name
-    self.detail = detail
-    self.url = url
-    self.image = image
-  }
-}
+//struct Track {
+//  var artist: String?
+//  var name: String?
+//  var image: UIImage?
+//
+//  init(artist: String? = nil, name: String? = nil, image: UIImage? = nil) {
+//    self.name = name
+//    self.artist = artist
+//    self.image = image
+//  }
+//}
+//
+//struct Station {
+//  let name: String
+//  let detail: String
+//  let url: URL
+//  var image: URL?
+//
+//  init(name: String, detail: String, url: URL, image: URL? = nil) {
+//    self.name = name
+//    self.detail = detail
+//    self.url = url
+//    self.image = image
+//  }
+//}
 
 protocol PlatformViewControllerDelegate {
   func didUpdateCounter(counter: Int)
@@ -41,22 +41,22 @@ class PlatformViewController : UIViewController {
   var delegate: PlatformViewControllerDelegate? = nil
   var counter: Int = 0
 
-  var track: Track? {
-    didSet {
-      incrementLabel.text = track?.name
-      updateNowPlaying(with: track)
-    }
-  }
-
-  // Singleton ref to player
-  let player: FRadioPlayer = FRadioPlayer.shared
-
-  // List of stations
-  let stations = [Station(name: "Newport Folk Radio",
-          detail: "Are you ready to Folk?",
-          url: URL(string: "http://rfcmedia.streamguys1.com/Newport.mp3")!,
-          image: URL(string: "http://xata44.by/sites/default/files/logo-radio.png")!),
-  ]
+//  var track: Track? {
+//    didSet {
+//      incrementLabel.text = track?.name
+//      updateNowPlaying(with: track)
+//    }
+//  }
+//
+//  // Singleton ref to player
+//  let player: FRadioPlayer = FRadioPlayer.shared
+//
+//  // List of stations
+//  let stations = [Station(name: "Newport Folk Radio",
+//          detail: "Are you ready to Folk?",
+//          url: URL(string: "http://rfcmedia.streamguys1.com/Newport.mp3")!,
+//          image: URL(string: "http://xata44.by/sites/default/files/logo-radio.png")!),
+//  ]
 
   @IBOutlet weak var incrementLabel: UILabel!
 
@@ -64,22 +64,22 @@ class PlatformViewController : UIViewController {
     super.viewDidLoad()
     setIncrementLabelText()
 
-    player.delegate = self
-
-    player.radioURL = URL(string: "https://s2.radio.co/se91d38a33/listen")
-    player.isAutoPlay = true
-    player.enableArtwork = true
-    player.artworkSize = 600
-    player.togglePlaying()
-
-    setupRemoteTransportControls()
+//    player.delegate = self
+//
+//    player.radioURL = URL(string: "https://s2.radio.co/se91d38a33/listen")
+//    player.isAutoPlay = true
+//    player.enableArtwork = true
+//    player.artworkSize = 600
+//    player.togglePlaying()
+//
+//    setupRemoteTransportControls()
   }
 
   func handleIncrement(_ sender: Any) {
     print("handleIncrement()")
     self.counter += 1
     self.setIncrementLabelText()
-    player.togglePlaying()
+//    player.togglePlaying()
   }
 
   func switchToFlutterView(_ sender: Any) {
@@ -94,128 +94,128 @@ class PlatformViewController : UIViewController {
 }
 
 // MARK: - FRadioPlayerDelegate
-extension PlatformViewController: FRadioPlayerDelegate {
-  func radioPlayer(_ player: FRadioPlayer, playerStateDidChange state: FRadioPlayerState) {
-    print("0")
-    print(state)
-    incrementLabel.text = state.description
-  }
-
-  func radioPlayer(_ player: FRadioPlayer, playbackStateDidChange state: FRadioPlaybackState) {
-//    playButton.isSelected = player.isPlaying
-    print(state)
-    print("1")
-  }
-
-  func radioPlayer(_ player: FRadioPlayer, metadataDidChange artistName: String?, trackName: String?) {
-      print("2")
-      print("artistName:")
-      print(artistName)
-      print("trackName:")
-      print(trackName)
-      track = Track(artist: artistName, name: trackName)
-  }
-
-  func radioPlayer(_ player: FRadioPlayer, itemDidChange url: URL?) {
-      print("3")
-      print("url:")
-      print(url)
-      track = nil
-  }
-
-  func radioPlayer(_ player: FRadioPlayer, metadataDidChange rawValue: String?) {
-      print("4")
-      print("rawValue:")
-      print(rawValue)
-//      infoContainer.isHidden = (rawValue == nil)
-  }
-
-  func radioPlayer(_ player: FRadioPlayer, artworkDidChange artworkURL: URL?) {
-    print("5")
-    print("artworkURL:")
-    print(artworkURL)
-    // Please note that the following example is for demonstration purposes only, consider using asynchronous network calls to set the image from a URL.
-    guard let artworkURL = artworkURL, let data = try? Data(contentsOf: artworkURL) else {
-//      artworkImageView.image = stations[selectedIndex].image
-      return
-    }
-    track?.image = UIImage(data: data)
-//    artworkImageView.image = track?.image
-    updateNowPlaying(with: track)
-  }
-}
+//extension PlatformViewController: FRadioPlayerDelegate {
+//  func radioPlayer(_ player: FRadioPlayer, playerStateDidChange state: FRadioPlayerState) {
+//    print("0")
+//    print(state)
+//    incrementLabel.text = state.description
+//  }
+//
+//  func radioPlayer(_ player: FRadioPlayer, playbackStateDidChange state: FRadioPlaybackState) {
+////    playButton.isSelected = player.isPlaying
+//    print(state)
+//    print("1")
+//  }
+//
+//  func radioPlayer(_ player: FRadioPlayer, metadataDidChange artistName: String?, trackName: String?) {
+//      print("2")
+//      print("artistName:")
+//      print(artistName)
+//      print("trackName:")
+//      print(trackName)
+//      track = Track(artist: artistName, name: trackName)
+//  }
+//
+//  func radioPlayer(_ player: FRadioPlayer, itemDidChange url: URL?) {
+//      print("3")
+//      print("url:")
+//      print(url)
+//      track = nil
+//  }
+//
+//  func radioPlayer(_ player: FRadioPlayer, metadataDidChange rawValue: String?) {
+//      print("4")
+//      print("rawValue:")
+//      print(rawValue)
+////      infoContainer.isHidden = (rawValue == nil)
+//  }
+//
+//  func radioPlayer(_ player: FRadioPlayer, artworkDidChange artworkURL: URL?) {
+//    print("5")
+//    print("artworkURL:")
+//    print(artworkURL)
+//    // Please note that the following example is for demonstration purposes only, consider using asynchronous network calls to set the image from a URL.
+//    guard let artworkURL = artworkURL, let data = try? Data(contentsOf: artworkURL) else {
+////      artworkImageView.image = stations[selectedIndex].image
+//      return
+//    }
+//    track?.image = UIImage(data: data)
+////    artworkImageView.image = track?.image
+//    updateNowPlaying(with: track)
+//  }
+//}
 
 // MARK: - Remote Controls / Lock screen
 
-extension PlatformViewController {
-
-  func setupRemoteTransportControls() {
-    print("setupRemoteTransportControls()")
-    // Get the shared MPRemoteCommandCenter
-    let commandCenter = MPRemoteCommandCenter.shared()
-
-    // Add handler for Play Command
-    commandCenter.playCommand.addTarget { [unowned self] event in
-      if self.player.rate == 0.0 {
-        self.player.play()
-        return .success
-      }
-      return .commandFailed
-    }
-
-    // Add handler for Pause Command
-    commandCenter.pauseCommand.addTarget { [unowned self] event in
-      if self.player.rate == 1.0 {
-        self.player.pause()
-        return .success
-      }
-      return .commandFailed
-    }
-
-    // Add handler for Next Command
-    commandCenter.nextTrackCommand.addTarget { [unowned self] event in
-//      self.next()
-      return .success
-    }
-
-    // Add handler for Previous Command
-    commandCenter.previousTrackCommand.addTarget { [unowned self] event in
-//      self.previous()
-      return .success
-    }
-  }
-
-  func updateNowPlaying(with track: Track?) {
-    print("updateNowPlaying(with track: Track?)")
-
-    // Define Now Playing Info
-    var nowPlayingInfo = [String : Any]()
-
-    if let artist = track?.artist {
-      nowPlayingInfo[MPMediaItemPropertyArtist] = artist
-    }
-
-    nowPlayingInfo[MPMediaItemPropertyTitle] = track?.name ?? stations[0].name
-
-    if #available(iOS 10.0, *) {
-      if let image = track?.image {
-        nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { _ -> UIImage in
-          return image
-        })
-      }
-    } else {
-
-    }
-
-    // Set the metadata
-    MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
-  }
-}
+//extension PlatformViewController {
+//
+//  func setupRemoteTransportControls() {
+//    print("setupRemoteTransportControls()")
+//    // Get the shared MPRemoteCommandCenter
+//    let commandCenter = MPRemoteCommandCenter.shared()
+//
+//    // Add handler for Play Command
+//    commandCenter.playCommand.addTarget { [unowned self] event in
+//      if self.player.rate == 0.0 {
+//        self.player.play()
+//        return .success
+//      }
+//      return .commandFailed
+//    }
+//
+//    // Add handler for Pause Command
+//    commandCenter.pauseCommand.addTarget { [unowned self] event in
+//      if self.player.rate == 1.0 {
+//        self.player.pause()
+//        return .success
+//      }
+//      return .commandFailed
+//    }
+//
+//    // Add handler for Next Command
+//    commandCenter.nextTrackCommand.addTarget { [unowned self] event in
+////      self.next()
+//      return .success
+//    }
+//
+//    // Add handler for Previous Command
+//    commandCenter.previousTrackCommand.addTarget { [unowned self] event in
+////      self.previous()
+//      return .success
+//    }
+//  }
+//
+//  func updateNowPlaying(with track: Track?) {
+//    print("updateNowPlaying(with track: Track?)")
+//
+//    // Define Now Playing Info
+//    var nowPlayingInfo = [String : Any]()
+//
+//    if let artist = track?.artist {
+//      nowPlayingInfo[MPMediaItemPropertyArtist] = artist
+//    }
+//
+//    nowPlayingInfo[MPMediaItemPropertyTitle] = track?.name ?? stations[0].name
+//
+//    if #available(iOS 10.0, *) {
+//      if let image = track?.image {
+//        nowPlayingInfo[MPMediaItemPropertyArtwork] = MPMediaItemArtwork(boundsSize: image.size, requestHandler: { _ -> UIImage in
+//          return image
+//        })
+//      }
+//    } else {
+//
+//    }
+//
+//    // Set the metadata
+//    MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+//  }
+//}
 
 // MARK: - UINavigationController
 
-extension UINavigationController {
-  open override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-}
+//extension UINavigationController {
+//  open override var preferredStatusBarStyle: UIStatusBarStyle {
+//    return .lightContent
+//  }
+//}
